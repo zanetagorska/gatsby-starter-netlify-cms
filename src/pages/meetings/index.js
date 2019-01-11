83efe4397ffdc5462172af7db152b5d2f3cf42af
zanetagorska <zaneta.gorska1@gmail.com>
2019-01-11 10:54:52 +0100

    Test request

--- src/pages/meetings/index.js
+++ src/pages/meetings/index.js
@@ -4,14 +4,14 @@ import Layout from '../../components/Layout';
 import Card from './Card';
 
 const MeetingPage = (props) => {
-	const meetings = props.data.allMarkdownRemark.edges;
+	console.log(props);
 	return (
 		<Layout>
 			<section className="section">
 				<div className="container">
 					<div className="content">
 						<h1 className="has-text-weight-bold is-size-2">Latest meetings</h1>
-						<div>{meetings.map((meeting) => <Card key={meeting.id} />)}</div>
+						{/* <div>{meetings.map((meeting) => <Card key={meeting.id} />)}</div> */}
 					</div>
 				</div>
 			</section>
@@ -23,21 +23,12 @@ export default MeetingPage;
 
 export const pageQuery = graphql`
 	query MeetupQuery {
-		allMarkdownRemark(
-			sort: { order: DESC, fields: [frontmatter___date] }
-			filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-		) {
+		allMarkdownRemark(filter: { frontmatter: { templateKey: { eq: "meetings" } } }) {
 			edges {
 				node {
-					excerpt(pruneLength: 400)
 					id
-					fields {
-						slug
-					}
 					frontmatter {
 						title
-						templateKey
-						date(formatString: "MMMM DD, YYYY")
 					}
 				}
 			}

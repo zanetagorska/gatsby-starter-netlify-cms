ad253933bcf707719306c283a52d9588b8739e8d
zanetagorska <zaneta.gorska1@gmail.com>
2019-01-14 10:33:23 +0100

    Create meeting component

---
+++ src/templates/meeting.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import Content, { HTMLContent } from '../components/Content';
+
+export const MeetingTemplate = ({ title, content, contentComponent }) => {
+	const PageContent = contentComponent || Content;
+
+	return (
+		<section className="section section--gradient">
+			<div className="container">
+				<div className="columns">
+					<div className="column is-10 is-offset-1">
+						<div className="section">
+							<h2 className="title is-size-3 has-text-weight-bold is-bold-light">{title}</h2>
+							<PageContent className="content" content={content} />
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+const MeetingPage = ({ data }) => {
+	const { markdownRemark: post } = data;
+	console.log(data);
+
+	return <Layout>Meeting Page</Layout>;
+};
+
+MeetingPage.propTypes = {
+	data: PropTypes.object.isRequired
+};
+
+export default MeetingPage;
+
+export const MeetingPageQuery = graphql`
+	query MeetingPage($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			html
+			frontmatter {
+				title
+			}
+		}
+	}
+`;

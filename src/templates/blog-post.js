b552c76595643c7d684a817256bc11044640745a
zanetagorska <zaneta.gorska1@gmail.com>
2019-01-17 14:34:27 +0100

    Edit blog

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { kebabCase } from 'lodash';
 import Helmet from 'react-helmet';
@@ -6,33 +6,36 @@ import { graphql, Link } from 'gatsby';
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
 
-export const BlogPostTemplate = ({ content, contentComponent, description, tags, title, helmet }) => {
+export const BlogPostTemplate = ({ content, contentComponent, description, tags, title, helmet, main_image }) => {
 	const PostContent = contentComponent || Content;
 	return (
-		<section className="section">
-			{helmet || ''}
-			<div className="container content">
-				<div className="columns">
-					<div className="column is-10 is-offset-1">
-						<h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
-						<p>{description}</p>
-						<PostContent content={content} />
-						{tags && tags.length ? (
-							<div style={{ marginTop: `4rem` }}>
-								<h4>Tags</h4>
-								<ul className="taglist">
-									{tags.map((tag) => (
-										<li key={tag + `tag`}>
-											<Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-										</li>
-									))}
-								</ul>
-							</div>
-						) : null}
+		<Fragment>
+			<img src={main_image} />
+			<section className="section">
+				{helmet || ''}
+				<div className="container content">
+					<div className="columns">
+						<div className="column is-10 is-offset-1">
+							<h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
+							<p>{description}</p>
+							<PostContent content={content} />
+							{tags && tags.length ? (
+								<div style={{ marginTop: `4rem` }}>
+									<h4>Tags</h4>
+									<ul className="taglist">
+										{tags.map((tag) => (
+											<li key={tag + `tag`}>
+												<Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+											</li>
+										))}
+									</ul>
+								</div>
+							) : null}
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</Fragment>
 	);
 };
 
@@ -63,6 +66,7 @@ const BlogPost = ({ data }) => {
 				}
 				tags={post.frontmatter.tags}
 				title={post.frontmatter.title}
+				main_image={post.frontmatter.main_image}
 			/>
 		</Layout>
 	);
